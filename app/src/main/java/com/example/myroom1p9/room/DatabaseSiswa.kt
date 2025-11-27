9db88f61a1d5e956bb56211abff607cbbea70124
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-27 09:29:20 +0700

    Tambah singleton getDatabase() untuk inisialisasi database

--- app/src/main/java/com/example/myroom1p9/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/myroom1p9/room/DatabaseSiswa.kt
@@ -1,6 +1,8 @@
 package com.example.myroom1p9.room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
@@ -8,4 +10,18 @@ abstract class DatabaseSiswa : RoomDatabase() {
 
     abstract fun siswaDao() : SiswaDao
 
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance ?: synchronized(lock = this){
+                Room.databaseBuilder(
+                    context,
+                    klass = DatabaseSiswa::class.java,
+                    name = "siswa_database"
+                ).build().also { Instance = it }
+            })
+        }
+    }
 }
