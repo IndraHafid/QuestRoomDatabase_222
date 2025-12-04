236e19bf7dbdbce32dd5ba897592e56097bb03a7
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 10:39:59 +0700

    Update SiswaDao: tambah getSiswa, delete, ubah insert jadi REPLACE, dan sorting by id

--- app/src/main/java/com/example/myroom1p9/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroom1p9/room/SiswaDao.kt
@@ -1,6 +1,7 @@
 package com.example.myroom1p9.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -8,9 +9,16 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
-    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
+    @Query("SELECT * FROM tblSiswa ORDER BY id ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Query("SELECT * FROM tblSiswa WHERE id = :id LIMIT 1")
+    fun getSiswa(id: Int): Flow<Siswa?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa:Siswa)
+
 }
