525a0c405158d3df73c118e31ed6d231e83dbc40
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 09:17:29 +0700

    Base ViewModel structure

--- app/src/main/java/com/example/myroom1p9/viewmodel/DetailSiswa.kt
+++ app/src/main/java/com/example/myroom1p9/viewmodel/DetailSiswa.kt
@@ -1,5 +1,16 @@
 package com.example.myroom1p9.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.myroom1p9.repositori.RepositoriSiswa
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+}
+
 /**
  * UI state for ItemDetailsScreen
  */
