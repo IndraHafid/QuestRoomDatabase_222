edc50f55062dd424630d021a379af85803fe4754
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 12:48:05 +0700

    Buat EditSiswaScreen dengan struktur Scaffold dan AppBar

---
+++ app/src/main/java/com/example/myroom1p9/view/HalamanEdit.kt
@@ -0,0 +1,32 @@
+package com.example.myroom1p9.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myroom1p9.view.route.DestinasiEditSiswa
+import com.example.myroom1p9.viewmodel.EditViewModel
+import com.example.myroom1p9.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEditSiswa.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+
+}
