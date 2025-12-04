fc158b24f5a57ee8b1f68e7d72b6fa423fd82384
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 12:49:57 +0700

    Tambahkan coroutineScope untuk operasi edit siswa

--- app/src/main/java/com/example/myroom1p9/view/HalamanEdit.kt
+++ app/src/main/java/com/example/myroom1p9/view/HalamanEdit.kt
@@ -3,6 +3,7 @@ package com.example.myroom1p9.view
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -28,5 +29,7 @@ fun EditSiswaScreen(
         },
         modifier = modifier
     ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+
 
 }
