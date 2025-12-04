138cb3434526490000f36918f4032c44c8485eb6
IndraHfid <indrahafidsaputra@gmail.com>
2025-12-04 12:44:00 +0700

    Tambahkan fungsi validasiInput untuk pengecekan form

--- app/src/main/java/com/example/myroom1p9/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myroom1p9/viewmodel/EditViewModel.kt
@@ -36,4 +36,10 @@ class EditViewModel(
             uiStateSiswa.copy(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
 }
